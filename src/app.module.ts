013505c0757b98214065d46ad6fd47056942ce34
karol-macas <klmacas@espe.edu.ec>
2024-02-29 23:02:05 -0500

    comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://root:example@localhost:27017'),
     PostsModule,
+    CommentsModule
   ],
 })
 export class AppModule {}
